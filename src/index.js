c08d534ab7b47fec2f38177f2b741c8cda520d29
alex <alexander.fedotov.uk@gmail.com>
2020-12-29 16:34:15 +0000

    Use * imports for exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-export game from "~/lib/game";
-export error from "~/lib/error";
-export lobby from "~/lib/lobby";
+import * as game from "./lib/game";
+import * as error from "./lib/error";
+import * as lobby from "./lib/lobby";
+
+export {game, lobby, error};
