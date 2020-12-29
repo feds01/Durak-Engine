f6728ce2515c50e0033a1bd5cf8a448123470fe7
alex <alexander.fedotov.uk@gmail.com>
2020-12-29 22:31:48 +0000

    export correct module for 'events'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as game from "./lib/game";
 import * as error from "./lib/error";
 import * as lobby from "./lib/lobby";
-import * as events from "./lib/lobby";
+import * as events from "./lib/events";
 
 export {game, lobby, events, error};
