eb91ed421a92f81046aa785883c917dfc48029ed
alex <alexander.fedotov.uk@gmail.com>
2020-12-29 20:48:16 +0000

    Added 'events' module to shared lib

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as game from "./lib/game";
 import * as error from "./lib/error";
 import * as lobby from "./lib/lobby";
+import * as events from "./lib/lobby";
 
-export {game, lobby, error};
+export {game, lobby, events, error};
---
+++ src/lib/events.js
@@ -0,0 +1,17 @@
+// server events
+export const JOIN_GAME = "join_game";
+export const UPDATE_PASSPHRASE = "update_passphrase";
+export const START_GAME = "start_game";
+export const TURN = "turn";
+export const NEXT_ROUND = "next_round";
+
+
+// client events
+export const JOINED_GAME = "joined_game";
+export const NEW_PLAYER = "new_player";
+export const COUNTDOWN = "countdown";
+export const GAME_STARTED = "game_started";
+
+
+// generic events
+export const ERROR = "error";
