fb7f2bb147f92f91c2f9bafb7d2d8271fac70742
alex <alexander.fedotov.uk@gmail.com>
2021-01-13 12:33:25 +0000

    remove lib/utils.js from source.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as game from "./lib/game";
 import * as error from "./lib/error";
-import * as lobby from "./lib/lobby";
+import * as utils from "./lib/utils";
 import * as events from "./lib/events";
 
-export {game, lobby, events, error};
+export {game, utils, events, error};
--- src/lib/events.js
+++ src/lib/events.js
@@ -2,7 +2,6 @@
 export const JOIN_GAME = "join_game";
 export const UPDATE_PASSPHRASE = "update_passphrase";
 export const START_GAME = "start_game";
-export const NEXT_ROUND = "next_round";
 export const KICK_PLAYER = "kick_player";
 export const MOVE = "move";
 
@@ -18,8 +17,6 @@ export const VICTORY = "victory";
 
 // @redundant event.
 export const UPDATED_PASSPHRASE = "updated_passphrase";
-
-// generic events
 
 // Event signifying that an error occurred on the server side.
 export const ERROR = "error";
