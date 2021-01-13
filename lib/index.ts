5d3c494694edfc10bfa1d2b76f0e61d029c130b2
alex <alexander.fedotov.uk@gmail.com>
2021-01-13 18:08:18 +0000

    move everything game protocol related into lib/protocol
    - convert enums into string enums for readability.

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,8 @@
 export * as error from './error';
 export {shuffleArray, getRandomKey} from "./utils";
-export {ServerEvents, ClientEvents} from "./events";
+export {Event, ServerEvents, MoveTypes, GameStatus, ClientEvents} from "./protocol";
 
 
 export {Game} from "./engine/game";
 export { CardType, parseCard} from "./engine/card";
 export {CardNumerics, CardSuits} from './engine/consts';
-
-/**
- * Possible move types that any player can make.
- * */
-export enum MoveTypes {
-    PLACE,
-    COVER,
-    FORFEIT,
-}
-
-
-export enum GameStatus {
-    WAITING,
-    STARTED,
-    PLAYING,
-}
